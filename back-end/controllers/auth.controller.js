8e31143173b65cd9fd674b76222faa225463fa95
Alexandre Larosa <alexandrelarosa.dev@outlook.fr>
2024-03-11 21:03:59 +0100

    Generate JWT

--- back-end/controllers/auth.controller.js
+++ back-end/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 export const signup = async (req, res) => {
   try {
@@ -29,7 +30,8 @@ export const signup = async (req, res) => {
     });
 
     if (newUser) {
-      
+      // Generate JWT token
+      generateTokenAndSetCookie(newUser._id, res);
       await newUser.save();
 
       res.status(201).json({
